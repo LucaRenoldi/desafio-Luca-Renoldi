c168dc091f51a5dba3e5c402be56f16134bda96f
LucaRenoldi <94394945+LucaRenoldi@users.noreply.github.com>
2023-08-16 20:18:40 -0300

    Update caixa-da-lanchonete.js

--- desafio-lanchonete-2023/src/caixa-da-lanchonete.js
+++ desafio-lanchonete-2023/src/caixa-da-lanchonete.js
@@ -1,40 +1,138 @@
 class CaixaDaLanchonete {
-
-    calcularValorDaCompra(metodoDePagamento, itens) {
-        let valorTotal = 0;
-        let resultado;
-        if(!itens){
-           return console.log("Não há itens no carrinho de compra!");
-        }
-
-        valorTotal = this.verificacaoItens(itens);
-        resultado = this.metodoDePagamento(metodoDePagamento, valorTotal);
-        return resultado;
+  
+  cardapio = {
+        'cafe': 3.00,
+        'chantily': 1.50,
+        'suco': 6.20,
+        'sanduiche': 6.50,
+        'queijo': 2.00, 
+        'salgado': 7.25,
+        'combo1': 9.50,
+        'combo2': 7.50
+      };
+  
+  calcularValorDaCompra(metodoDePagamento, itens) {
+    
+    let verificacao = this.verificarItens(itens);
+    
+    if(verificacao == "Tudo certo!"){
+      let resultado, valorTotal = 0, separacao;
+            
+      for(let i = 0; i < itens.length; i++){
+        separacao = itens[i].split(",");
+        valorTotal += this.cardapio[separacao[0]] * separacao[1];
+      }
+      
+      resultado = this.pagamento(metodoDePagamento, valorTotal);
+      console.log(resultado);
+      return resultado;
+    }
+    else{
+      return verificacao;
     }
     
-        metodoDePagamento(metodoDePagamento, valorTotal){
-        switch(metodoDePagamento){
-            case "dinheiro":
-                valorTotal = valorTotal - (valorTotal * 0.05);
-                return "R$" + valorTotal.toString();
-                break;
-            case "debito":
-                valorTotal = valorTotal - (valorTotal * 0.03);
-                return "R$" + valorTotal.toString();
-                break;
-            case "credito":
-                return "R$" + valorTotal.toString();
-                break;
-            default:
-                return "Forma de pagamento inválida!";
-        }
-    }
+  }
 
-    verificacaoItens(itens){
+  pagamento(metodoDePagamento, valorPagamento) {
+    let valorTotal;
+    switch (metodoDePagamento) {
+      case "dinheiro":
+        valorPagamento = valorPagamento * 0.95;
+        valorTotal = valorPagamento.toFixed(2);
+        return "R$ " + valorTotal.toString().replace(".", ",");
+      case "debito":
+        valorTotal = valorPagamento.toFixed(2);
+        return "R$ " + valorTotal.toString().replace(".", ",");
+      case "credito":
+        valorPagamento = valorPagamento * 1.03
+        valorTotal = valorPagamento.toFixed(2)
+        return "R$ " + valorTotal.toString().replace(".", ",");
+      default:
+        return "Forma de pagamento inválida!";
+    }
+  }
+
+  verificarItens(itens) {
+    if(itens.length == 0) {
+      return "Não há itens no carrinho de compra!";
+    }
+    else if(this.verificarCodigo(itens) == false){
+      return "Item inválido!"
+    }
+    else if(this.verificarQuantidade(itens) == false){
+      return "Quantidade inválida!";
+    }
+    else if(this.verificarAdicionais(itens) == false){
+      return "Item extra não pode ser pedido sem o principal";
+    }
+    else{
+      return "Tudo certo!"
+    } 
+  }
+
+  verificarAdicionais(itens) {
+    let complementares = {
+      "chantily": false,
+      "cafe": false,
+      "sanduiche": false,
+      "queijo": false
+    };
+    let separacao = "";
+    for (let i = 0; i < itens.length; i++) {
+      separacao = itens[i].split(",");
+      switch (separacao[0]) {
+        case "chantily":
+          complementares[separacao[0]] = true;
+          break;
+        case "sanduiche":
+          complementares[separacao[0]] = true;
+          break;
+        case "queijo":
+          complementares[separacao[0]] = true;
+          break;
+        case "cafe":
+          complementares[separacao[0]] = true;
+          break;
+        default:
+          break;
+      }
+    }
+    if (complementares["cafe"] == false && complementares["chantily"] == true) {
+      return false;
+    }
+    else if (complementares["sanduiche"] == false && complementares["queijo"] == true) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
+
+  verificarQuantidade(itens) {
+    let separacao = "";
+    for (let i = 0; i < itens.length; i++) {
+      separacao = itens[i].split(",");
+      if (separacao[1] < 1) {
+        return false;
         
+      }
     }
-
-
+    return true;
+  }
+  
+  verificarCodigo(itens){
+    
+    let separacao;
+    
+    for (let i = 0; i < itens.length; i++) {
+      separacao = itens[i].split(",");
+      if (separacao[0]  in this.cardapio){
+        return true;
+      }
+    }
+    
+    return false;
+  }
 }
 
 export { CaixaDaLanchonete };
